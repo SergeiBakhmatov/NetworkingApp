ed67bdd424c8af82e50046bd8ddf888d40f23297
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-07 11:51:33 +0500

    Add model

---
+++ NetworkingApp/Models/Fruit.swift
@@ -0,0 +1,15 @@
+//
+//  Fruit.swift
+//  NetworkingApp
+//
+//  Created by Sergei Bakhmatov on 07.05.2023.
+//
+
+struct Fruit {
+    
+    let name: String
+    let family: String
+    let genus: String
+    let order: String
+    
+}
